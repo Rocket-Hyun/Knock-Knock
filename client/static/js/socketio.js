690bf17abd31020db2c0e70814b8e610babce50b
Rocket-Hyun <rockethyun@gmail.com>
2017-12-23 15:22:55 +0900

    swl

--- client/static/js/socketio.js
+++ client/static/js/socketio.js
@@ -35,6 +35,25 @@ socket.on('deleteMarker', function(data){
 socket.on('recieveRequest', function(data){
   console.log("채팅창 요청!");
   console.log(data);
+
+  swal({
+    title: '채팅 요청',
+    text: data.requestId + "님께서 요청을 보내셨습니다.<br> 수락하시겠습니까?",
+    type: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.value) {
+      swal(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
+
 });
 
 // 포지션이 변경될 때 소켓ID랑 위치정보를 보냄
@@ -46,4 +65,16 @@ function sendPosition(position){
 // 채팅 Request 보내기
 function sendRequest(id){
   socket.emit('sendRequest',{requestId:mySocketId, receiveId:id});
+  swal({
+    title: '요청을 보냈습니다.',
+    text: '대기 중...',
+    timer: 5000,
+    onOpen: () => {
+      swal.showLoading()
+    }
+  }).then((result) => {
+    // if (result.dismiss === 'timer') {
+      console.log('상대방이 응답하지 않아 취소됐습니다!')
+    // }
+  })
 }
